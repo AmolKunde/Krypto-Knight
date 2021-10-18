ceaa463a9429d9b18e771db795fd26b4f28ed8b7
AMOL KUNDE <amolkunde20@gmail.com>
2021-10-18 15:05:08 +0530

    initial complete -1

---
+++ src/components/Predictions.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Predictions = () => {
+    return (
+        <div>
+            Predictions
+        </div>
+    )
+}
+
+export default Predictions
---
+++ src/services/exchangeApi.js
@@ -0,0 +1,5 @@
+// https://api.exchangeratesapi.io/v1/convert
+//     ? access_key = API_KEY
+//     & from = USD
+//     & to = INR
+//     & amount = 1
