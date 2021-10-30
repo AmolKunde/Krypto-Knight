ae7843ae781112dbbe88a0275b944401479168db
AMOL KUNDE <amolkunde20@gmail.com>
2021-10-30 22:09:34 +0530

    bug fixes

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -9,8 +9,8 @@ import { useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } from '../services/
 import Loader from './Loader'
 import LineChart from './LineChart'
 
-const { Title, Text } = Typography;
-const { Option } = Select;
+const { Title, Text } = Typography
+const { Option } = Select
 
 const CryptoDetails = () => {
   const { coinId } = useParams()
@@ -21,7 +21,7 @@ const CryptoDetails = () => {
 
   if (isFetching) return <Loader />
 
-  const time = ['3h', '24h', '7d', '30d', '3m', '1y', '3y', '5y']
+  const time = ['24h', '7d', '30d', '1y', '5y']
 
   const stats = [
     { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
@@ -47,7 +47,7 @@ const CryptoDetails = () => {
         </Title>
         <p>{cryptoDetails.name} live price in US Dollar (USD). View value statistics, market cap and supply.</p>
       </Col>
-      <Select defaultValue="24h" className="select-timeperiod" placeholder="Select Timeperiod" onChange={(value) => setTimeperiod(value)}>
+      <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Timeperiod" onChange={(value) => setTimeperiod(value)}>
         {time.map((date) => <Option key={date}>{date}</Option>)}
       </Select>
       <LineChart coinHistory={coinHistory} currentPrice={millify(cryptoDetails.price)} coinName={cryptoDetails.name} />
@@ -102,4 +102,4 @@ const CryptoDetails = () => {
   )
 }
 
-export default CryptoDetails;
+export default CryptoDetails
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Line } from 'react-chartjs-2';
-import { Col, Row, Typography } from 'antd';
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+import { Col, Row, Typography } from 'antd'
 
-const { Title } = Typography;
+const { Title } = Typography
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
-  const coinPrice = [];
-  const coinTimestamp = [];
+  const coinPrice = []
+  const coinTimestamp = []
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinPrice.push(coinHistory?.data?.history[i].price);
+    coinPrice.push(coinHistory?.data?.history[i].price)
   }
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
@@ -27,7 +27,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
         borderColor: '#0071bd',
       },
     ],
-  };
+  }
 
   const options = {
     scales: {
@@ -39,7 +39,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
         },
       ],
     },
-  };
+  }
 
   return (
     <>
@@ -52,7 +52,7 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
       </Row>
       <Line data={data} options={options} />
     </>
-  );
-};
+  )
+}
 
-export default LineChart;
+export default LineChart
